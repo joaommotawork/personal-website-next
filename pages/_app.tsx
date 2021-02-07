5ffb3c14ab9b580c0f4e6e41608e53129aa18c87
João Mota <joaommota.work@gmail.com>
2021-02-07 19:23:54 +0000

    Open Graph Image Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,7 +50,7 @@ const App = ({ Component, pageProps }) => {
                 />
                 <meta
                     property="og:image"
-                    content="/static/images/DefaultLogo.png"
+                    content="https://github.com/joaommotawork/joaommota-next-website/blob/main/public/static/images/DefaultLogo.png"
                     key="ogimage"
                 />
                 <meta
@@ -80,7 +80,7 @@ const App = ({ Component, pageProps }) => {
                 />
                 <meta
                     property="twitter:image"
-                    content="/static/images/DefaultLogo.png"
+                    content="https://github.com/joaommotawork/joaommota-next-website/blob/main/public/static/images/DefaultLogo.png"
                 />
                 <link
                     rel="apple-touch-icon"
