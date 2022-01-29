e7b24bcf642d0f3bfdba730337bcba531c2c5e1f
João Mota <joaommota.work@gmail.com>
2022-01-29 16:18:44 +0000

    fix(contacts): small fixes

--- components/pages/Contacts/index.tsx
+++ components/pages/Contacts/index.tsx
@@ -40,7 +40,7 @@ const Contacts = ({ t }) => {
         <div
             id="Contacts"
             className={`relative bg-background-pattern bg-fixed bg-cover flex flex-col items-center justify-center p-5 lg:flex-row md:px-32 pb-32`}>
-            <div className="flex flex-col items-center justify-center w-2/3 mb-10 md:p-16">
+            {/* <div className="flex flex-col items-center justify-center w-2/3 mb-10 md:p-16">
                 <div className="text-xl font-bold text-center text-white uppercase md:text-4xl">
                     {t('Contacts.CV')}
                 </div>
@@ -68,17 +68,17 @@ const Contacts = ({ t }) => {
                         </button>
                     </a>
                 </div>
-            </div>
+            </div> */}
             <form
                 className="flex flex-col w-full"
                 onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col w-full lg:flex-row">
                     <div className="flex-col w-full">
                         <div className="w-full lg:pr-3">
-                            <label className="block mb-2 text-xs font-bold tracking-wide text-white uppercase font-open">
+                            <label className="block mb-2 text-white font-open text-xs font-bold tracking-wide uppercase">
                                 {t('Contacts.Name')}{' '}
                                 {errors.Name && (
-                                    <span className="text-xs italic text-red-600">
+                                    <span className="text-red-600 text-xs italic">
                                         {t('Contacts.FieldRequired')}
                                     </span>
                                 )}
@@ -93,10 +93,10 @@ const Contacts = ({ t }) => {
                             />
                         </div>
                         <div className="w-full lg:pr-3">
-                            <label className="block mb-2 text-xs font-bold tracking-wide text-white uppercase font-open">
+                            <label className="block mb-2 text-white font-open text-xs font-bold tracking-wide uppercase">
                                 {t('Contacts.E-Mail')}{' '}
                                 {errors['E-Mail'] && (
-                                    <span className="text-xs italic text-red-600">
+                                    <span className="text-red-600 text-xs italic">
                                         {t('Contacts.FieldRequired')}
                                     </span>
                                 )}
@@ -111,10 +111,10 @@ const Contacts = ({ t }) => {
                             />
                         </div>
                         <div className="w-full lg:pr-3">
-                            <label className="block mb-2 text-xs font-bold tracking-wide text-white uppercase font-open">
+                            <label className="block mb-2 text-white font-open text-xs font-bold tracking-wide uppercase">
                                 {t('Contacts.Subject')}{' '}
                                 {errors.Subject && (
-                                    <span className="text-xs italic text-red-600">
+                                    <span className="text-red-600 text-xs italic">
                                         {t('Contacts.FieldRequired')}
                                     </span>
                                 )}
@@ -129,11 +129,11 @@ const Contacts = ({ t }) => {
                             />
                         </div>
                     </div>
-                    <div className="w-full mb-9 lg:pl-3">
-                        <label className="block mb-2 text-xs font-bold tracking-wide text-white uppercase font-open">
+                    <div className="mb-9 w-full lg:pl-3">
+                        <label className="block mb-2 text-white font-open text-xs font-bold tracking-wide uppercase">
                             {t('Contacts.Message')}{' '}
                             {errors.Name && (
-                                <span className="text-xs italic text-red-600">
+                                <span className="text-red-600 text-xs italic">
                                     {t('Contacts.FieldRequired')}
                                 </span>
                             )}
@@ -151,8 +151,8 @@ const Contacts = ({ t }) => {
                 {isSuccessfullySubmitted && (
                     <div className="mt-3 text-center">
                         <div className="p-2">
-                            <div className="inline-flex items-center p-2 text-sm leading-none text-white bg-green-500 rounded-full shadow text-teal font-open">
-                                <span className="inline-flex items-center justify-center h-6 px-3 text-white bg-green-300 rounded-full">
+                            <div className="text-teal inline-flex items-center p-2 text-white font-open text-sm leading-none bg-green-500 rounded-full shadow">
+                                <span className="inline-flex items-center justify-center px-3 h-6 text-white bg-green-300 rounded-full">
                                     {t('Contacts.Success')}
                                 </span>
                                 <span className="inline-flex px-2">
@@ -163,7 +163,7 @@ const Contacts = ({ t }) => {
                     </div>
                 )}
                 <button
-                    className="w-full px-4 py-2 mt-5 font-bold text-white uppercase bg-red-600 font-open"
+                    className="mt-5 px-4 py-2 w-full text-white font-open font-bold bg-red-600 uppercase"
                     type="submit">
                     {t('Contacts.Send')}
                 </button>
